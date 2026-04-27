8252b691b39f92f5c59e29768c752f9a7066f037
josehelias <joseheliascc@gmail.com>
2026-04-27 13:51:33 -0300

    Versão Docker e Postgresql

--- agendamento/settings.py
+++ agendamento/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('STR_SECRET_KEY') == 'True'
-
+SECRET_KEY = os.getenv('STR_SECRET_KEY', 'chave-temporaria-so-para-desenvolvimento-123')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('STR_DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
