9a6be1609a436e6b8d492921f1464282e249df55
josehelias <joseheliascc@gmail.com>
2026-04-28 14:49:56 -0300

    Correção emergencial - Erro de direcionamento para a tela de agendamento

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('agendamentos/', views.servicos, name='agendamentos'),
+    path('agendamentos/', views.agendamentos, name='agendamentos'),
     path('servicos/', views.servicos, name='servicos'),
     path('contato/', views.contato, name='contato'),
     path('buscar-horarios/', views.buscar_horarios, name='buscar_horarios'),
